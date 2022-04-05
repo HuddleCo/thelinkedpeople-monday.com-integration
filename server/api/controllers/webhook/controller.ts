e8bde6de40f8eeb19832b0ff67131b3b7ef8aad3
David Milanese <milanese.david@gmail.com>
2022-04-05 15:43:04 +0930

    fix: replace query params with body request params.

--- server/api/controllers/webhook/controller.ts
+++ server/api/controllers/webhook/controller.ts
@@ -3,13 +3,12 @@ import l from '../../../common/logger';
 
 export class Controller {
   post(req: Request, res: Response): void {
-    l.debug(`params: ${JSON.stringify(req.params)}`);
     if (req.params.authToken !== process.env.AUTH_TOKEN) {
       res.status(401).json({ message: 'authToken unrecognised' });
       return;
     }
 
-    l.debug(`query: ${JSON.stringify(req.query)}`);
+    l.debug(`body: ${JSON.stringify(req.body)}`);
     res.status(200).json({ message: 'ok' });
   }
 }
