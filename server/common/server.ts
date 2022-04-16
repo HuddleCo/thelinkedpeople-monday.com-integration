312732e93584e2a03fc56e7bc32edf74e98078ab
David Milanese <milanese.david@gmail.com>
2022-04-16 23:43:12 +0930

    feat: add Sentry issue montiroing and tracking

--- server/common/server.ts
+++ server/common/server.ts
@@ -3,12 +3,24 @@ import path from 'path';
 import bodyParser from 'body-parser';
 import http from 'http';
 import * as OpenApiValidator from 'express-openapi-validator';
-import l from './logger';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 
+import l from './logger';
 import errorHandler from '../api/middlewares/error.handler';
 
 const limit = process.env.REQUEST_LIMIT || '100kb';
 const app = express();
+
+Sentry.init({
+  dsn:
+    process.env.SENTRY_DSN || 'https://examplePublicKey@o0.ingest.sentry.io/0',
+  environment: process.env.NODE_ENV || 'development',
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+});
 
 export default class ExpressServer {
   private routes: (app: Application) => void;
@@ -41,7 +53,10 @@ export default class ExpressServer {
   }
 
   router(routes: (app: Application) => void): ExpressServer {
+    app.use(Sentry.Handlers.requestHandler() as express.RequestHandler);
+    app.use(Sentry.Handlers.tracingHandler());
     routes(app);
+    app.use(Sentry.Handlers.errorHandler() as express.ErrorRequestHandler);
     app.use(errorHandler);
     return this;
   }
