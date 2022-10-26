f6dc24a5bb771748e296d17bd9bf940d482aa319
Harsh khare <harshkhare3@gmail.com>
2022-10-26 11:36:35 +1030

    fix: added a code block that checks if the API Key exists

--- server/api/services/convertAddress.service.ts
+++ server/api/services/convertAddress.service.ts
@@ -1,20 +1,27 @@
 import axios from 'axios';
 
 class Geocode {
+  private request = async (address: string) =>
+    await axios({
+      url: `https://api.tomtom.com/search/2/geocode/${encodeURIComponent(
+        address
+      )}.json`,
+      method: 'GET',
+      params: { key: process.env.TOMTOM_API_KEY },
+    });
+
   convertAddress = async (address: string | undefined) => {
     if (address !== undefined) {
-      const res = await axios({
-        url: `https://api.tomtom.com/search/2/geocode/${encodeURIComponent(
-          address
-        )}.json`,
-        method: 'GET',
-        params: { key: process.env.TOMTOM_API_KEY },
-      });
-      return {
-        lat: res.data.results[0].position.lat,
-        lng: res.data.results[0].position.lon,
-        address: address,
-      };
+      if (process.env.TOMTOM_API_KEY) {
+        const res = await this.request(address);
+        return {
+          lat: res.data.results[0].position.lat,
+          lng: res.data.results[0].position.lon,
+          address: address,
+        };
+      } else {
+        throw new Error('TOMTOM API Key missing!');
+      }
     } else {
       return {};
     }
