912f3407ab8bd0cd4d615fc290a0cdb88ef90c2c
David Milanese <milanese.david@gmail.com>
2022-11-10 17:09:26 +1030

    feat: add connection for vanessa and elizabeth

--- server/api/controllers/webhook/controller.ts
+++ server/api/controllers/webhook/controller.ts
@@ -25,6 +25,16 @@ const database: Array<Record> = [
     authToken: process.env.KRISTI_AUTH_TOKEN || '',
     mondayAuthToken: process.env.THECOACHINGDIRECTORY_MONDAY_AUTH_TOKEN || '',
   },
+  {
+    boardId: 3503954392,
+    authToken: process.env.ELIZABETH_AUTH_TOKEN || '',
+    mondayAuthToken: process.env.THECOACHINGDIRECTORY_MONDAY_AUTH_TOKEN || '',
+  },
+  {
+    boardId: 3503956486,
+    authToken: process.env.VANESSA_AUTH_TOKEN || '',
+    mondayAuthToken: process.env.THECOACHINGDIRECTORY_MONDAY_AUTH_TOKEN || '',
+  },
 ];
 
 const query = `
